4a3e1afd356c5794be1d5445b8c9ae1101d522f6
shubhaseesh <shubhaseesh09@gmail.com>
2022-11-14 14:01:07 +0530

    added redux-persist functionality

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import notesReducer from "../features/notes/notesSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
+const reducers = combineReducers({
+  notes: notesReducer,
+})
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-const store = configureStore({
-  reducer: { notes: notesReducer },
+const persistedReducer = persistReducer(persistConfig, reducers);
+export default configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
-
-export default store;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import { Provider } from "react-redux";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./app/store";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
+let persistor = persistStore(store);
 
 
 const container = document.getElementById("root");
@@ -14,7 +17,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
